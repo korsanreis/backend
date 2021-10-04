1dc20753063b0c7f7f9274e16b27ff09f9fefc42
korsanreis <celikerdem111@gmail.com>
2021-10-05 01:54:29 +0300

    backend

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -5,8 +5,8 @@ const checkAuth = require("../middlewares/check-auth");
 
 const router = express.Router();
  
-router.post("/signup", checkAuth, UserController.signup);
+router.post("/signup", UserController.signup);
 router.post("/login", UserController.login);
-router.get("/:userId", checkAuth, UserController.getOneUser);
+router.get("/:userId",  UserController.getOneUser);
 
 module.exports = router;
